7ef72ac3a747c4b7a844856a89cfce2a7c3a056f
Yuta <yuta.shogai@gmail.com>
2021-05-27 19:16:49 +0900

    Ruby入門 第2-3章終了

---
+++ 第2章/Sample2_3_1.rb
@@ -0,0 +1,17 @@
+# 2_3_1
+
+puts "こんにちは\nさようなら"
+puts 'こんにちは\nさようなら'
+puts "こんにちは\\nさようなら"
+
+name = 'Alice'
+puts "Hello, #{name}!"
+puts 'Hello, #{name}!'
+puts 'Hello, ' + name + '!'
+puts "Hello, \#{name}!"
+
+i = 10
+puts "#{i}は16進数にすると#{i.to_s(16)}です"
+
+puts 'He said, "Don\'t speak."'
+puts "He said, \"Don't speak.\""
---
+++ 第2章/Sample2_3_2.rb
@@ -0,0 +1,14 @@
+# 2_3_2
+
+puts 'ruby' == 'ruby'
+puts 'ruby' == 'Ruby'
+puts 'ruby' != 'perl'
+puts 'ruby' != 'ruby'
+
+puts 'a' < 'b'
+puts 'a' < 'A'
+puts 'a' > 'A'
+puts 'abc' < 'def'
+puts 'abc' < 'ab'
+puts 'abc' < 'abcd'
+puts 'あいうえお' < 'かきくけこ'
