3c07d82690a0af2db6dfac4a022fe2ce330b868c
Yuta <yuta.shogai@gmail.com>
2021-06-03 19:42:54 +0900

    Ruby入門 第2-9章終了

---
+++ 第2章/Sample2_9_1.rb
@@ -0,0 +1,5 @@
+puts 0b1111111111
+
+puts 0357
+
+puts 0xfe
---
+++ 第2章/Sample2_9_4.rb
@@ -0,0 +1,23 @@
+puts 355.class
+puts 2.54.class
+puts 12345678901234567890.class
+
+r = 5/4r
+puts r
+puts r.class
+
+f = 5/4
+puts f
+puts f.class
+
+r = '6/5'.to_r
+puts r
+puts r.class
+
+c = 0.4 + 0.7i
+puts c
+puts c.class
+
+c2 = '0.3-0.8i'.to_c
+puts c2
+puts c2.class
