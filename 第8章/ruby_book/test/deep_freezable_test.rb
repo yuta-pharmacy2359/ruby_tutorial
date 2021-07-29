f56f301380360bd305a55b4535830a984a36df90
Yuta <yuta.shogai@gmail.com>
2021-07-29 23:58:10 +0900

    Ruby入門 第8-4章終了

--- 第8章/ruby_book/test/deep_freezable_test.rb
+++ 第8章/ruby_book/test/deep_freezable_test.rb
@@ -8,4 +8,13 @@ class DeepFreezableTest < Minitest::Test
     assert Team::COUNTRIES.frozen?
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freeze_to_hash
+    assert_equal(
+      { 'Japan' => 'yen', 'USA' => 'dollar', 'India' => 'rupee', 'Germany' => 'euro' },
+      Bank::CURRENCIES
+    )
+    assert Bank::CURRENCIES.frozen?
+    assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
+  end
 end
